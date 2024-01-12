4f0f6b489c78bf391637237245c2eb1590c7ac8d
Tạ Quang Chính <tqchinh@vnpt.vn>
2024-01-12 12:17:02 +0700

    change

--- xchange-okex/src/main/java/org/knowm/xchange/okex/dto/trade/OkexOrderFlags.java
+++
@@ -1,9 +0,0 @@
-package org.knowm.xchange.okex.dto.trade;
-
-import org.knowm.xchange.dto.Order;
-
-public enum OkexOrderFlags implements Order.IOrderFlags {
-  POST_ONLY,
-  REDUCE_ONLY,
-  OPTIMAL_LIMIT_IOC
-}
